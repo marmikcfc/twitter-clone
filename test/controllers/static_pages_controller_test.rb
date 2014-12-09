7f6efc8ed6c0572838fae88ef2293b6497ae5de0
marmikcfc <marmik.pandya@capgemini.com>
2014-12-09 08:53:03 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,21 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | twitter clone"
+
   end
 
   test "should get help" do
     get :help
     assert_response :success
-  end
+    assert_select "title", "Help | twitter clone"
 
+  end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | twitter clone"
+
+  end
 end
